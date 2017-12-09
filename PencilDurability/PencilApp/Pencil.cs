0628062f70df142f79c8609ff6d843274e85adb2
Kevin Ure <Kevin.M.Ure@gmail.com>
2017-12-09 14:49:58 -0500

    Code cleanup and commenting

--- PencilDurability/PencilApp/Pencil.cs
+++ PencilDurability/PencilApp/Pencil.cs
@@ -9,8 +9,17 @@ namespace PencilApp
 {
     public class Pencil
     {
+        /// <summary>
+        /// The reminaing durability of the pencil tip. Without durability the pencil writes only spaces.
+        /// </summary>
         public int Durability;
+        /// <summary>
+        /// The durability the pencil started with. this is the new durability when sharpened
+        /// </summary>
         private int initialDurability;
+        /// <summary>
+        /// current lenght of the pencil. a pencil with zero length will not sharpen
+        /// </summary>
         public int Length;
 
         public Pencil (int startingDurability, int startingLength)
@@ -20,6 +29,11 @@ namespace PencilApp
             Length = startingLength;
         }
 
+        /// <summary>
+        /// appends a string to another string so long as the pencil has durability to use. Adds spaces if not.
+        /// </summary>
+        /// <param name="inputText">The text to be written by the pencil</param>
+        /// <param name="existingText">Where to write the text to</param>
         public void Write(string inputText, ref string existingText)
         {
             if (inputText == null)
@@ -32,9 +46,9 @@ namespace PencilApp
                 else if (inputText[i] == ' ')
                     neededDurability = 0;
                 else
-                    neededDurability = 1;
+                    neededDurability = 1; //should cover both lower case and punctuation
                 if (Durability - neededDurability < 0)
-                    existingText += " ";
+                    existingText += " "; //write spaces if durability runs out
                 else
                 {
                     existingText += inputText[i];
@@ -43,6 +57,9 @@ namespace PencilApp
             }
         }
 
+        /// <summary>
+        /// Restores the pencil's original durability at the cost of one unit of length
+        /// </summary>
         public void Sharpen()
         {
             if(Length > 0)
